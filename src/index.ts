b9a0da99427c59dfd93bf7f0806b8ae84106e06b
JEFFe <jeffe@jeffe.co>
2022-09-05 21:54:36 +0300

    Timezone ajoituksia

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { startServer } from './server'
 import * as dotenv from 'dotenv'
+import setTZ from 'set-tz'
 dotenv.config()
+
+setTZ('Europe/Helsinki')
 
 const main = async () => {
   console.log('Trollaus alkakoon')
